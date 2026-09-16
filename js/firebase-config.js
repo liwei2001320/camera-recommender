e8ddb3fed5b120ffed723b60486a3e92ffc8dd7f
liwei2001320 <liwei2001320@gmail.com>
2026-09-16 20:26:07 +0800

    Add firebase config for levi-camera

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAgOxcdIDERdunQgsP36Aw55C5DCIUv6-o",
+  authDomain: "levi-camera.firebaseapp.com",
+  projectId: "levi-camera",
+  storageBucket: "levi-camera.firebasestorage.app",
+  messagingSenderId: "776310910252",
+  appId: "1:776310910252:web:9afe4751b7c18a8e0a9089",
+  measurementId: "G-793TMTGB11"
+};
